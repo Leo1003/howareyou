01c1cdaa041fdef3f8a45583a021b8b2cd68f378
Leo <leo881003@gmail.com>
2021-09-15 16:35:47 +0800

    Fix echo server issues

--- src/routes.rs
+++ src/routes.rs
@@ -1,4 +1,4 @@
-use futures::{FutureExt, StreamExt};
+use futures::{future, FutureExt, StreamExt, TryStreamExt};
 use std::convert::Infallible;
 use warp::hyper::StatusCode;
 use warp::reply::{json, with_status};
@@ -24,11 +24,13 @@ pub fn ws(ws: Ws) -> impl Reply {
         info!("New Websocket connection created!");
         // Pipe the message back
         let (tx, rx) = ws.split();
-        rx.forward(tx).map(|result| {
-            if let Err(e) = result {
-                error!("websocket error: {:?}", e);
-            }
-            debug!("Websocket connection closed");
-        })
+        rx.try_filter(|msg| future::ready(msg.is_text() || msg.is_binary()))
+            .forward(tx)
+            .map(|result| {
+                if let Err(e) = result {
+                    error!("websocket error: {:?}", e);
+                }
+                debug!("Websocket connection closed");
+            })
     })
 }
